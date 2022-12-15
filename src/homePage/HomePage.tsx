5b8ca3643771face23c46bab385dc3c6ef02c67c
aasmal97 <74555081+aasmal97@users.noreply.github.com>
2022-12-15 00:24:46 -0500

    fix title in index.html

--- src/homePage/HomePage.tsx
+++ src/homePage/HomePage.tsx
@@ -31,6 +31,7 @@ const HomePage = () => {
       <div className={`${namespace}-inner-container`}>
         <HomePageNav />
         <IntroBanner />
+        
         <HomePageCategoriesNav />
         <SecondaryBanner />
       </div>
