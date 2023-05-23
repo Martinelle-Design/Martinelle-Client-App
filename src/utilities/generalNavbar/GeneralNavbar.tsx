b998b548cd89e7a275c97a1ef7c57eca555a346a
Arky Asmal <arkyasmal@gmail.com>
2023-05-23 18:23:04 -0400

    change styling and remove search bar

--- src/utilities/generalNavbar/GeneralNavbar.tsx
+++ src/utilities/generalNavbar/GeneralNavbar.tsx
@@ -1,87 +1,93 @@
 import { Button } from "@mui/material";
 import { faSearch, faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState, useRef, useEffect, RefObject } from "react";
-import { createPortal } from "react-dom";
+import {
+  useState,
+  useEffect,
+  //useRef,
+  //RefObject
+} from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../utilities/logo.svg";
-import useDebouncedTextInput from "../../hooks/use-debounced-text-input";
-import useWindowWidth from "../../hooks/use-window-width";
-import useClickOutside from "../../hooks/use-click-outside";
+
 import Drawer from "../drawer/Drawer";
 import { pages } from "../secondaryNavbar/SecondaryNavbar";
+// import { createPortal } from "react-dom";
+// import useDebouncedTextInput from "../../hooks/use-debounced-text-input";
+// import useWindowWidth from "../../hooks/use-window-width";
+// import useClickOutside from "../../hooks/use-click-outside";
 const namespace = "general-navbar";
 
 const searchFunc = (str: string) => {};
-const SearchBar = ({ navbarRef }: { navbarRef: HTMLElement | null }) => {
-  const { textValue, onChange } = useDebouncedTextInput({
-    debouncedFunc: searchFunc,
-    time: 400,
-  });
-  const smallWindowWidth = useWindowWidth(576);
-  const [search, setSearch] = useState(false);
-  const searchUpdatingRef = useRef(false);
-  const {
-    ref: searchContainerRef,
-    isClickOutside,
-    setisClickOutside,
-  } = useClickOutside(false);
-  useEffect(() => {
-    if (!isClickOutside && !searchUpdatingRef.current)
-      setSearch((state) => {
-        if (state) return !state;
-        else return state;
-      });
-  }, [isClickOutside, search]);
-  const largeWidthBtn = (
-    <button aria-label="search catalog" onClick={() => searchFunc(textValue)}>
-      <FontAwesomeIcon icon={faSearch} />
-    </button>
-  );
-  const smallWidthBtn = (
-    <button
-      aria-label="search catalog"
-      onClick={() =>
-        setSearch((state) => {
-          searchUpdatingRef.current = true;
-          setTimeout(() => {
-            searchUpdatingRef.current = false;
-            if (!state) setisClickOutside(true);
-          }, 0);
-          return !state;
-        })
-      }
-    >
-      <FontAwesomeIcon icon={faSearch} />
-    </button>
-  );
-  const inputEl = (
-    <input
-      type={"text"}
-      value={textValue}
-      onChange={onChange}
-      placeholder={"Search furniture catalog"}
-    />
-  );
+// const SearchBar = ({ navbarRef }: { navbarRef: HTMLElement | null }) => {
+//   const { textValue, onChange } = useDebouncedTextInput({
+//     debouncedFunc: searchFunc,
+//     time: 400,
+//   });
+//   const smallWindowWidth = useWindowWidth(576);
+//   const [search, setSearch] = useState(false);
+//   const searchUpdatingRef = useRef(false);
+//   const {
+//     ref: searchContainerRef,
+//     isClickOutside,
+//     setisClickOutside,
+//   } = useClickOutside(false);
+//   useEffect(() => {
+//     if (!isClickOutside && !searchUpdatingRef.current)
+//       setSearch((state) => {
+//         if (state) return !state;
+//         else return state;
+//       });
+//   }, [isClickOutside, search]);
+//   const largeWidthBtn = (
+//     <button aria-label="search catalog" onClick={() => searchFunc(textValue)}>
+//       <FontAwesomeIcon icon={faSearch} />
+//     </button>
+//   );
+//   const smallWidthBtn = (
+//     <button
+//       aria-label="search catalog"
+//       onClick={() =>
+//         setSearch((state) => {
+//           searchUpdatingRef.current = true;
+//           setTimeout(() => {
+//             searchUpdatingRef.current = false;
+//             if (!state) setisClickOutside(true);
+//           }, 0);
+//           return !state;
+//         })
+//       }
+//     >
+//       <FontAwesomeIcon icon={faSearch} />
+//     </button>
+//   );
+//   const inputEl = (
+//     <input
+//       type={"text"}
+//       value={textValue}
+//       onChange={onChange}
+//       placeholder={"Search furniture catalog"}
+//     />
+//   );
 
-  return (
-    <div className={`${namespace}-search-container`}>
-      {!smallWindowWidth ? smallWidthBtn : largeWidthBtn}
-      {smallWindowWidth && inputEl}
-      {!smallWindowWidth &&
-        navbarRef &&
-        createPortal(
-          <div
-            ref={searchContainerRef as RefObject<HTMLDivElement>}
-            className={`${namespace}-search-input${search ? " show" : ""}`}
-          >
-            {inputEl}
-          </div>,
-          navbarRef
-        )}
-    </div>
-  );
-};
+//   return (
+//     <div className={`${namespace}-search-container`}>
+//       {!smallWindowWidth ? smallWidthBtn : largeWidthBtn}
+//       {smallWindowWidth && inputEl}
+//       {!smallWindowWidth &&
+//         navbarRef &&
+//         createPortal(
+//           <div
+//             ref={searchContainerRef as RefObject<HTMLDivElement>}
+//             className={`${namespace}-search-input${search ? " show" : ""}`}
+//           >
+//             {inputEl}
+//           </div>,
+//           navbarRef
+//         )}
+//     </div>
+//   );
+// };
 const NavLinks = ({
   toggleDrawer,
 }: {
@@ -161,7 +167,7 @@ const GeneralNavBar = ({ toggleBtn }: { toggleBtn?: boolean }) => {
         {<Logo height={"100%"} />}
       </Link>
       <div className={`${namespace}-inner`}>
-        <SearchBar navbarRef={navbarRef} />
+        {/* <SearchBar navbarRef={navbarRef} /> */}
         <Link to="/contact" aria-label="contact">
           {"Contact".toUpperCase()}
         </Link>
